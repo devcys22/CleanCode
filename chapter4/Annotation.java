c361b5dcaf193600498ffcbeb82cba456978ce56
dev22 <ysoli36512@gmail.com>
2023-05-04 07:22:53 +0900

    ✨ feature: 네이밍 변경 & 좋은주석

---
+++ chapter4/Annotation.java
@@ -0,0 +1,17 @@
+class Annotation{
+
+    MockRequest request;
+    private final String HTTP_DATE_REGEXP =
+            "[SMTWF][a-z]{2}\\,\\s[0-9]{2}\\s[JFMASOND][a-z]{2}\\s"
+    private Response response;
+    private Locale saveLocale;
+    //Example: "Tue, 02 Apr "
+
+    // 직원에게 복지 혜택을 받을 자격이 있는지 검사한다.
+    if((employee.flags & HOURLY_FLAG) &&
+            (employee.age > 65))
+    if(employee.isEligibleForFullBenefits())
+
+
+    //좋은 주석은 주석을 달지 않을 방법을 찾아낸 주석!
+}
